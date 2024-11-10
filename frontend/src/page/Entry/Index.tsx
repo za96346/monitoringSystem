4f79b361df18530771824b8cf02c5106eadd046f
siou <za96346@gmail.com>
2024-11-10 16:11:33 +0800

    chore: 前端調整登入畫面

--- frontend/src/page/Entry/Index.tsx
+++ frontend/src/page/Entry/Index.tsx
@@ -1,9 +1,41 @@
+import { LockOutlined, UserOutlined } from "@ant-design/icons"
+import { Button, Input } from "antd"
 import * as React from "react"
+import { useNavigate } from "react-router-dom"
 
 const Index = () => {
+    const navigate = useNavigate()
     return (
-        <div>
-            This is entry page.
+        <div style={{
+            backgroundColor: "rgba(255,255,255,0.5)",
+            borderRadius: "10px",
+            padding: "15px",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "400px",
+            height: "600px"
+        }}>
+            <Input
+                prefix={<UserOutlined />}
+                placeholder="使用者帳號"
+                size="large"
+                style={{ marginBottom: "5px",  }}
+            />
+            <Input
+                prefix={<LockOutlined />}
+                placeholder="使用者密碼"
+                size="large"
+                style={{ marginBottom: "5px" }}
+            />
+            <Button
+                onClick={() => {
+                    navigate("/dataMonitor")
+                }}
+                >
+                    登入
+            </Button>
         </div>
     )
 }
