52d7174373cf74a066804152d3ccfe49de2a5fe8
siou <za96346@gmail.com>
2024-11-11 19:43:47 +0800

    chore: 加上domain layer 注入

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,10 +11,23 @@ import EntryApp from "./appService/EntryApp"
 // 基礎設施層
 import Presistence from "./infrastructure/presistence/index"
 
+// 領域層
+import UserPo from "./domain/po/UserPo"
+import DevicePo from "./domain/po/DevicePo"
+import DeviceDataPo from "./domain/po/DeviceDataPo"
+import DeviceDomainService from './domain/service/DeviceDomainService';
+
 const app = express();
-const presistence = Presistence()
+const presistence = Presistence({
+    type: "mysql",
+    logging: ['error', 'query', 'schema'],
+    entities: [DevicePo, UserPo, DeviceDataPo]
+})
 const appServiceInstance = {
-    DeviceApp: new DeviceApp({ deviceRepo: presistence.Device }),
+    DeviceApp: new DeviceApp({
+        deviceRepo: presistence.Device,
+        deviceDomainService: new DeviceDomainService()
+    }),
     DataMonitorApp: new DataMonitorApp(),
     EntryApp: new EntryApp()
 }
@@ -25,7 +38,6 @@ const webSocketPort = 3001
 ApiServer({
     app,
     port: apiPort,
-    presistence,
     appService: appServiceInstance
 })
 WebSocketServer({
