38fad401d8b3df265a153acf05c64bcc6e810b4e
twgpu <54166251+twgpu@users.noreply.github.com>
2024-11-26 20:44:14 +0800

    finish @todo

--- backend/src/appService/UserApp.ts
+++ backend/src/appService/UserApp.ts
@@ -1,3 +1,9 @@
+import UserEntity from "domain/entity/UserEntity"
+import { AppService } from "types/AppService";
+import { Repository } from "types/Repository";
+import { domainService } from "types/domainService";
+
+
 /**
  * @todo step 2. 此物件實作你定義的使用者介面
  * 可以參考appService底下的檔案
@@ -7,8 +13,42 @@
  * 轉換函式已經實作完成，只需要注入userDomainService物件，並調用 toPersistenceObject
  * 
 */
-class UserApp {
+class UserApp implements AppService.UserApp{
+    userRepo: Repository.User
+    userDomainService: domainService.UserDomainService
 
+    constructor(
+        {
+            userRepo,
+            userDomainService
+        }:{
+            userRepo: Repository.User,
+            userDomainService: domainService.UserDomainService
+        }
+    ) {
+        this.userRepo = userRepo
+        this.userDomainService = userDomainService
+    }
+
+    async add(userEntity: UserEntity): Promise<UserEntity> {
+        const userPo = this.userDomainService.toPersistenceObject(userEntity)
+        const result = await this.userRepo.add(userPo)
+        return this.userDomainService.toDomainEntity(result)
+    }
+    async delete(userEntity: UserEntity): Promise<boolean> {
+        const userPo = this.userDomainService.toPersistenceObject(userEntity)
+        return await this.userRepo.delete(userPo)
+    }
+    async update(userEntity: UserEntity): Promise<UserEntity> {
+        const userPo = this.userDomainService.toPersistenceObject(userEntity)
+        const result = await this.userRepo.update(userPo)
+        return this.userDomainService.toDomainEntity(result)
+    }
+    async get(userEntity: UserEntity): Promise<UserEntity[]> {
+        const result = await this.userRepo.getUsers()
+        return result.map((item) => this.userDomainService.toDomainEntity(item))
+    }
+    
 }
 
 export default UserApp
