ffe44d1cebee867b7982196b9cf78cce319ebf4a
XUQIANLIU <paulliu00426@gmail.com>
2025-01-10 22:21:21 +0800

    Login Page Style Changed

--- frontend/src/page/Entry/Index.tsx
+++ frontend/src/page/Entry/Index.tsx
@@ -2,68 +2,63 @@ import { LockOutlined, UserOutlined } from "@ant-design/icons"
 import { Button, Form, Input } from "antd"
 import * as React from "react"
 import { useNavigate } from "react-router-dom"
-import "./index.scss"
+// import "./index.scss"
 
 const Index = ({ userApi }: { userApi: Api.User }) => {
     const navigate = useNavigate()
     return (
-            
-            <Form
-                onFinish={(v) => {
-                    userApi.login(v).then(() => {
-                        navigate("/dataMonitor")
-                    })
-                }}
-                
-                style={{
-                    backgroundColor: "rgba(255,255,255,0.5)",
-                    borderRadius: "10px",
-                    padding: "15px",
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    width: "400px",
-                    height: "500px",
-                    boxShadow: "0 8px 32px rgba(0, 0, 0, 0.25)"
-                }}
-            >
-                <h1>LOG IN</h1>
-                
-                <Form.Item className="" name="account">
-                    <label>username</label>
-                    <Input
-                        // prefix={<UserOutlined />}
-                        // placeholder="使用者帳號"
-                        size="large"
-                        name="account"
-                        style={{ marginBottom: "5px", }}
-                    />
-                </Form.Item>
-                
-                <Form.Item className="" name="password">
-                    <label>password</label>
-                    <Input
-                        // prefix={<LockOutlined />}
-                        // placeholder="使用者密碼"
-                        name="password"
-                        type="password"
-                        size="large"
-                        style={{ marginBottom: "5px" }}
-                    />
-                </Form.Item>
-                <Form.Item >
-                    <Button htmlType="submit" className="glow-on-hover">
-                        登入
-                    </Button>
-                    
-                    
-                </Form.Item>
-                <div className="bar">
-                    <div className="ball"></div>
+        <section>
+            <div className="color"></div>
+            <div className="color"></div>
+            <div className="color"></div>
+            <div className="box">
+                <div className="square" style={{ "--i": "0" } as React.CSSProperties}></div>
+                <div className="square" style={{ "--i": "1" } as React.CSSProperties}></div>
+                <div className="square" style={{ "--i": "2" } as React.CSSProperties}></div>
+                <div className="square" style={{ "--i": "3" } as React.CSSProperties}></div>
+                <div className="square" style={{ "--i": "4" } as React.CSSProperties}></div>
+
+                <div className="container">
+                    <div className="form">
+                        <h2>Login Form</h2>
+                        <Form
+                            onFinish={(v) => {
+                                userApi.login(v).then(() => {
+                                    navigate("/dataMonitor")
+                                })
+                            }}
+                        >
+                            <div className="inputBox">
+                                <Form.Item name="account">
+                                    <Input
+                                        placeholder="Username"
+                                        size="large"
+                                        name="account"
+                                    />
+                                </Form.Item>
+                            </div>
+
+                            <div className="inputBox">
+                                <Form.Item name="password">
+                                    <Input
+                                        placeholder="Password"
+                                        name="password"
+                                        type="password"
+                                        size="large"
+                                    />
+                                </Form.Item>
+                            </div>
+
+                            
+                                <Form.Item>
+                                    <Button htmlType="submit"  className="glow-on-hover">登入</Button>
+                                </Form.Item>
+                            
+                        </Form>
+                    </div>
                 </div>
-            </Form>
-        
+            </div>
+        </section>
     )
 }
 
